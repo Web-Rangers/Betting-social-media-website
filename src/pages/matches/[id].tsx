e833af02f49985dc6b3101afea4072dc5932dadf
MaX2708 <grin.maxim.2708@yandex.ru>
2022-08-18 19:15:00 +0400

    match summary with first tab

--- src/pages/matches/[id].tsx
+++ src/pages/matches/[id].tsx
@@ -1,11 +1,69 @@
-import type { NextPage } from "next"
+import type { NextComponentType, NextPage } from "next"
 import Head from "next/head"
 import styles from "../../styles/pages/MatchSummary.module.css"
 import Image from "next/image"
-import { useRouter } from "next/router";
+import { useRouter } from "next/router"
+import React, { useEffect, useState } from "react"
+import { motion, AnimatePresence } from "framer-motion"
+
+const pages = [
+    {
+        id:1,
+        name:"Match Summary"
+    },
+    {
+        id:2,
+        name:"Lineups"
+    },
+    {
+        id:3,
+        name:"Odds"
+    },
+    {
+        id:4,
+        name:"H2H"
+    },
+    {
+        id:5,
+        name:"Standings"
+    },
+    {
+        id:6,
+        name:"Predictions"
+    },
+    {
+        id:7,
+        name:"Statistics"
+    },
+    {
+        id:8,
+        name:"Match Summary"
+    },
+    ,
+    {
+        id:9,
+        name:"Bookmakers"
+    }    
+]
 
 const MatchSummary: NextPage = () => {
     const router = useRouter()
+    const [selectedPage, setSelectedPage] = useState(0)
+    const [selectedPageComponent, setSelectedPageComponent] = useState(<MatchSummaryPage />)
+
+    useEffect(()=>{
+        switch (selectedPage) {
+            case 0:
+                setSelectedPageComponent(<MatchSummaryPage />)
+                break;
+            case 1:
+                setSelectedPageComponent(<LineupsPage />)
+                break;
+            case 2:
+                setSelectedPageComponent(<OddsPage />)
+                break;
+        }
+    },[selectedPage])
 
     return (
         <>
@@ -92,10 +150,162 @@ const MatchSummary: NextPage = () => {
                             </div>
                         </div>
                     </div>
+
+                    <div className={styles.matchStatPages}>
+                        <div className={styles.pageSelector}>
+                            {pages.map((page, index) => (
+                                <span 
+                                    className={`${styles.page} ${index == selectedPage && styles.pageActive}`}
+                                    onClick={()=>setSelectedPage(index)}
+                                    key={page?.id}
+                                    aria-label={page?.name}
+                                >
+                                    {page?.name}
+                                    {index == selectedPage && 
+                                        <motion.div className={styles.pageUnderline} layoutId="pageUnderline" />
+                                    }
+                                </span>
+                            ))}
+                        </div>
+                        <AnimatePresence exitBeforeEnter>
+                            <motion.div
+                                key={selectedPage ? selectedPage : "empty"}
+                                initial={{ y: 10, opacity: 0 }}
+                                animate={{ y: 0, opacity: 1 }}
+                                exit={{ y: -10, opacity: 0 }}
+                                transition={{ duration: 0.2 }}
+                            >
+                                {selectedPageComponent}
+                            </motion.div>
+                        </AnimatePresence>
+                    </div>
                 </main>
             </div>
         </>
     );
 };
+
+const MatchSummaryPage: React.FC = () => {
+    return (
+        <div className={styles.pageContainer}>
+            <div className={styles.stageStat}>
+                <div className={styles.stage}>
+                    <span>
+                        1ST HALF
+                    </span>
+                    <span>
+                        0 - 0
+                    </span>
+                </div>
+                <div className={styles.statLine}>
+                    <span>
+                        13'
+                    </span>
+                    <div>
+                        ICON
+                    </div>                    
+                    <span>
+                        Some Action
+                    </span>
+                </div>
+                <div className={styles.statLine} style={{flexDirection:'row-reverse'}}>
+                    <span>
+                        26'
+                    </span>
+                    <div>
+                        ICON
+                    </div>                    
+                    <span>
+                        Some Action
+                    </span>
+                </div>
+                <div className={styles.stage}>
+                    <span>
+                        2ND HALF
+                    </span>
+                    <span>
+                        0 - 0
+                    </span>
+                </div>
+                <div className={styles.statLine}>
+                    <span>
+                        56'
+                    </span>
+                    <div>
+                        ICON
+                    </div>                    
+                    <span>
+                        Some Action
+                    </span>
+                </div>
+                <div className={styles.statLine}>
+                    <span>
+                        56'
+                    </span>
+                    <div>
+                        ICON
+                    </div>                    
+                    <span>
+                        Some Action
+                    </span>                   
+                    <span style={{fontWeight:400}}>
+                        Some Action
+                    </span>
+                </div>
+                <div className={styles.statLine}>
+                    <span>
+                        56'
+                    </span>
+                    <div className={styles.iconWithText}>
+                        <div>ICON</div>
+                        1 - 0
+                    </div>                    
+                    <span>
+                        Some Action
+                    </span>
+                </div>
+                <div className={styles.statLine} style={{flexDirection:'row-reverse'}}>
+                    <span>
+                        78'
+                    </span>
+                    <div>
+                        ICON
+                    </div>                    
+                    <span>
+                        Some Action
+                    </span>
+                </div>
+                <div className={styles.statLine} style={{flexDirection:'row-reverse'}}>
+                    <span>
+                        85'
+                    </span>
+                    <div className={styles.iconWithText} style={{flexDirection:'row-reverse'}}>
+                        <div>ICON</div>
+                        1 - 0
+                    </div>                    
+                    <span>
+                        Some Action
+                    </span>
+                </div>
+            </div>            
+        </div>
+    )
+}
+
+const LineupsPage: React.FC = () => {
+    return (
+        <div className={styles.pageContainer}>
+            
+        </div>
+    )
+}
+
+const OddsPage: React.FC = () => {
+    return (
+        <div className={styles.pageContainer}>
+            
+        </div>
+    )
+}
 
 export default MatchSummary;
