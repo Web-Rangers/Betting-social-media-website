4dacfc2efca9b9adf252b12d91b2cb98bea74592
NikitoriusMinsk <45353693+NikitoriusMinsk@users.noreply.github.com>
2022-08-23 15:54:09 +0400

    adjust position: relative on home page

--- src/pages/tipster-rating/index.tsx
+++ src/pages/tipster-rating/index.tsx
@@ -1,13 +1,29 @@
-import React, { useState } from 'react'
+import React, { createContext, useContext, useMemo, useState } from 'react'
 import styles from "@styles/pages/TipsterRating.module.css"
 import { inferQueryOutput, trpc } from 'src/utils/trpc'
 import Image from 'next/image';
 import Slider from '@components/ui/Slider';
 import type { Tipsters } from 'src/types/queryTypes';
 import { inferArrayElementType } from 'src/utils/inferArrayElementType';
+import * as portals from 'react-reverse-portal';
+
+interface IPortalContext {
+    portalNode: portals.HtmlPortalNode | null
+}
+
+const PortalContext = createContext<IPortalContext>({ portalNode: null });
 
 const TipsterRating: React.FC = () => {
     const { data: tipsters, isLoading: tipstersLoading } = trpc.useQuery(['tipsters.getAll']);
+    const isSSR = typeof window === "undefined";
+
+    const portalNode = useMemo(() => {
+        if (isSSR) {
+            return null;
+        }
+        return portals.createHtmlPortalNode({ attributes: { style: "position: absolute; top: 0; left: 0;" } });
+    }, []);
+
 
     if (tipstersLoading) {
         return <div>Loading...</div>
@@ -19,14 +35,17 @@ const TipsterRating: React.FC = () => {
 
     return (
         <>
-            <div className={styles.mainBlock}>
-                <VerifiedTipsters tipsters={tipsters} />
-            </div>
+            <PortalContext.Provider value={{ portalNode: portalNode }}>
+                {portalNode && <portals.OutPortal node={portalNode} />}
+                <div className={styles.mainBlock}>
+                    <VerifiedTipsters tipsters={tipsters} portalNode={portalNode} />
+                </div>
+            </PortalContext.Provider>
         </>
     )
 }
 
-const VerifiedTipsters: React.FC<{ tipsters: Tipsters }> = (props) => {
+const VerifiedTipsters: React.FC<{ tipsters: Tipsters, portalNode: portals.HtmlPortalNode | null }> = (props) => {
     const { tipsters } = props;
     const _tipsters = sliceIntoChunks(tipsters, 3);
 
@@ -75,59 +94,68 @@ const VerifiedTipsters: React.FC<{ tipsters: Tipsters }> = (props) => {
 const TipsterCard: React.FC<inferArrayElementType<Tipsters>> = (props) => {
     const { avgProfit, image, name, subscribtionCost, winrate, sport } = props
     const [following, setFollowing] = useState(false);
+    const portalCotext = useContext(PortalContext)
 
     return (
-        <div className={styles.tipsterCard}>
-            <div className={styles.mainInfo}>
-                <div className={styles.avatar}>
-                    <Image
-                        src={image}
-                        height={80}
-                        width={80}
-                    />
-                    <span>{name}</span>
+        <>
+            {portalCotext?.portalNode &&
+                <portals.InPortal node={portalCotext.portalNode}>
+                    Hello!
+                </portals.InPortal>
+            }
+            <div className={styles.tipsterCard}>
+                <div className={styles.mainInfo}>
+                    <div className={styles.avatar}>
+                        <Image
+                            src={image}
+                            height={80}
+                            width={80}
+                        />
+                        <span>{name}</span>
+                    </div>
+                    <div className={styles.info}>
+                        <div className={styles.header}>
+                            <div className={styles.sport}>
+                                <Image
+                                    src={sport.image}
+                                    height={46}
+                                    width={46}
+                                />
+                                <span>Top {sport.name} Tipster</span>
+                            </div>
+                            <button onClick={() => setFollowing(!following)}>
+                                <Image
+                                    src={following ? '/icons/following.svg' : '/icons/follow.svg'}
+                                    height={20}
+                                    width={20}
+                                />
+                                <span>{following ? 'Following' : 'Follow'}</span>
+                            </button>
+                        </div>
+                        <div className={styles.stats}>
+                            <div>
+                                <h4>Hit rate</h4>
+                                <span>{winrate * 100}%</span>
+                            </div>
+                            <div>
+                                <h4>Avg. Mothly Profit</h4>
+                                <span>$ {avgProfit}</span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className={styles.info}>
-                    <div className={styles.header}>
-                        <div className={styles.sport}>
-                            <Image
-                                src={sport.image}
-                                height={46}
-                                width={46}
-                            />
-                            <span>Top {sport.name} Tipster</span>
-                        </div>
-                        <button onClick={() => setFollowing(!following)}>
-                            <Image
-                                src={following ? '/icons/following.svg' : '/icons/follow.svg'}
-                                height={20}
-                                width={20}
-                            />
-                            <span>{following ? 'Following' : 'Follow'}</span>
-                        </button>
+                <div className={styles.subscriptionInfo}>
+                    <div>
+                        <h4>$ {subscribtionCost}/MO</h4>
+                        <span>How to subscribe?</span>
                     </div>
-                    <div className={styles.stats}>
-                        <div>
-                            <h4>Hit rate</h4>
-                            <span>{winrate * 100}%</span>
-                        </div>
-                        <div>
-                            <h4>Avg. Mothly Profit</h4>
-                            <span>$ {avgProfit}</span>
-                        </div>
-                    </div>
+                    <button>
+                        Subscribe
+                    </button>
                 </div>
             </div>
-            <div className={styles.subscriptionInfo}>
-                <div>
-                    <h4>$ {subscribtionCost}/MO</h4>
-                    <span>How to subscribe?</span>
-                </div>
-                <button>
-                    Subscribe
-                </button>
-            </div>
-        </div>
+        </>
+
     )
 }
 
