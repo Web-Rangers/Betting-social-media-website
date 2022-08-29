06bcc7fb7887d0afc9aca924b48f3eb50eafa3bc
GuBeLa <irakligubelidze@gmail.com>
2022-08-29 09:44:51 +0400

    change matchs page

---
+++ src/components/ui/MatchesInfo.tsx
@@ -0,0 +1,179 @@
+import React, { useState } from 'react'
+import styles from '@styles/components/ui/MatchesInfo.module.css'
+import Image from 'next/image'
+import { Predictions } from 'src/types/queryTypes'
+import { inferArrayElementType } from 'src/utils/inferArrayElementType'
+import { motion } from 'framer-motion'
+
+interface MatchesInfoProps {
+    leagues: Predictions,
+    h3?: string,
+    h2?: string,
+}
+
+type MatchType = inferArrayElementType<inferArrayElementType<Predictions>['matches']>
+
+const MatchesInfo: React.FC<MatchesInfoProps> = (props) => {
+    const { leagues, h2, h3 } = props
+
+    return (
+        <div className={styles.container}>
+            {(h2 || h3) && <div className={styles.titles}>
+                {h3 && <h3>{h3}</h3>}
+                {h2 && <h2>{h2}</h2>}
+            </div>}
+            {leagues.map((league, leagueIndex) =>
+                <div className={styles.league} key={`league_${leagueIndex}`}>
+                    <div className={styles.leagueDescription}>
+                        <div className={styles.image}>
+                            <Image
+                                src={league.image}
+                                height={40}
+                                width={40}
+                                alt={league.name}
+                            />
+                        </div>
+                        <div className={styles.titles}>
+                            <span>{league.country} • {league.sport.name}</span>
+                            <span>{league.name}</span>
+                        </div>
+                        <div className={styles.matchesOptions}>
+                            test
+                        </div>
+                    </div>
+                    <div className={styles.matches}>
+                        {league.matches.map((match, index) => (
+                            <Match {...match} key={`league_${leagueIndex}_match_${index}`} />
+                        ))}
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
+
+const MatchesInfoVariants = {
+    open: {
+        height: 'auto',
+        transition: {
+            duration: 0.3,
+            ease: 'easeInOut'
+        }
+    },
+    closed: {
+        height: 0,
+        transition: {
+            duration: 0.3,
+            ease: 'easeInOut'
+        }
+    }
+}
+
+const Match: React.FC<MatchType> = (props) => {
+    const { predictions, teams, time } = props
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <div className={styles.match}>
+            <div className={styles.header}>
+                <div className={styles.info}>
+                    <div className={styles.time}>
+                        <span>{time}</span>
+                        <span>Today</span>
+                        {/* <div className={styles.matchDuration}>Live</div> */}
+                    </div>
+                    <div className={styles.teamImages}>
+                        {teams.map((team, index) => (
+                            <div key={index} className={styles.teamImage}>
+                                <Image
+                                    src={team.image}
+                                    alt={team.name}
+                                    width={22}
+                                    height={22}
+                                />
+                            </div>
+                        ))}
+                    </div>
+                    <div className={styles.teamNames}>
+                        {teams.map((team, index) => (
+                            <div key={index} className={styles.teamName}>
+                                {team.name}
+                            </div>
+                        ))}
+                    </div>
+                    
+                </div>
+                <div className={styles.details}>
+                    <div className={`${styles.outcome} ${styles.score}`}>
+                        <span>0</span>
+                        <span>0</span>
+                    </div>
+                    <div className={styles.outcome}>
+                        <span>Home</span>
+                        <span>34%</span>
+                    </div>
+                    <div className={styles.outcome}>
+                        <span>Draw</span>
+                        <span>17%</span>
+                    </div>
+                    <div className={styles.outcome}>
+                        <span>Away</span>
+                        <span>49%</span>
+                    </div>
+                    <div
+                        className={`${styles.total} ${isOpen ? styles.open : styles.closed}`}
+                        onClick={() => setIsOpen(!isOpen)}
+                    >
+                        {predictions.length} Tip{predictions.length > 1 ? 's' : ''}
+                    </div>
+                </div>
+            </div>
+            <motion.div
+                className={styles.predictions}
+                variants={MatchesInfoVariants}
+                animate={isOpen ? 'open' : 'closed'}
+                initial={false}
+            >
+                {predictions.map((prediction, index) => (
+                    <div key={index} className={styles.prediction}>
+                        <div className={styles.info}>
+                            <div className={styles.time}>
+                                <span>{prediction.time}</span>
+                                <span>Today</span>
+                            </div>
+                            <div className={styles.user}>
+                                <div className={styles.userImage}>
+                                    <Image
+                                        src={prediction.user.image}
+                                        alt={prediction.user.name}
+                                        width={40}
+                                        height={40}
+                                    />
+                                </div>
+                                <div className={styles.userInfo}>
+                                    <div className={styles.userName}>{prediction.user.name}</div>
+                                    <div className={styles.userWinrate}>Winrate {prediction.user.winrate * 100}%</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.comment}>
+                            <Image
+                                src="/icons/comment.svg"
+                                alt="Comment"
+                                width={20}
+                                height={20}
+                            />
+                            <span>{prediction.comment ? 'With Comment' : 'Without comment'}</span>
+                        </div>
+                        <div className={styles.outcome}>
+                            <span>{prediction.type} Prediction</span>
+                            <span>{prediction.outcome}</span>
+                        </div>
+                    </div>
+                ))}
+            </motion.div>
+        </div>
+    )
+}
+
+export default MatchesInfo;
---
+++ src/pages/matches/index.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+import { NextPage } from 'next'
+import styles from '@styles/pages/Predictions.module.css'
+import { trpc } from 'src/utils/trpc'
+import { MostTips, Sports } from 'src/types/queryTypes'
+import Slider from '@components/ui/Slider'
+import MatchTipsCard from '@components/ui/MatchTipsCard'
+import Image from 'next/image'
+import BestBookmakers from '@components/ui/BestBookmakers'
+import Banner from '@components/ui/Banner'
+import Filter from '@components/ui/Filter'
+import TextField from '@components/ui/TextField'
+import DatePicker from '@components/ui/DatePicker'
+import LiveMatches from '@components/ui/LiveMatches'
+import MatchesInfo from '@components/ui/MatchesInfo'
+
+const MatchesPage: NextPage = () => {
+    const { data: tips, isLoading: tipsLoading } = trpc.useQuery(['tips.getAll'])
+    const { data: predictions, isLoading: predictionsLoading } = trpc.useQuery(['predictions.getAll'])
+    const { data: bookmakers, isLoading: bookmakersLoading } = trpc.useQuery(['bookmakers.getAll'])
+    const { data: leagues, isLoading: leaguesLoading } = trpc.useQuery(['filters.getLeagues'])
+    const { data: sports, isLoading: sportsLoading } = trpc.useQuery(['filters.getSports'])
+    const { data: matches, isLoading: matchesLoading } = trpc.useQuery(['matches.getAllLive'])
+
+    if (tipsLoading || predictionsLoading || bookmakersLoading || leaguesLoading || sportsLoading) {
+        return <div>Loading...</div>
+    }
+
+    if (!tips || !predictions || !bookmakers || !leagues || !sports) {
+        return <div>Error...</div>
+    }
+
+    return (
+        <>
+            <div className={styles.mainColumn}>
+                <div className={styles.filters}>
+                    <DatePicker onChange={() => { }} />
+                    <Filter
+                        items={leagues}
+                        h3="BY COUNTRY"
+                        onChange={() => { }}
+                    />
+                    <Filter
+                        items={leagues}
+                        h3="OTHER COUNTRIES"
+                        onChange={() => { }}
+                    />
+                </div>
+                <div className={styles.predictions}>
+                    <MatchesInfo leagues={predictions} />
+                </div>
+            </div>
+            {/* <div className={styles.mainBlock}>
+                <Banner height={200} image="/images/banner-placeholder-1.png" />
+            </div> */}
+            <div className={styles.sideColumn}>
+                <LiveMatches matches={matches} />
+                <Banner height={463} image='/images/banner-placeholder-2.png' />
+                <BestBookmakers bookmakers={bookmakers} />
+            </div>
+        </>
+    )
+}
+
+export default MatchesPage
