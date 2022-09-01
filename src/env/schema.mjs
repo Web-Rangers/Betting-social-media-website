e0e4acefd935a01fdaf7af600295c3d513bc2a21
NikitoriusMinsk <45353693+NikitoriusMinsk@users.noreply.github.com>
2022-09-01 15:48:05 +0400

    fix env schema

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -7,7 +7,7 @@ import { z } from "zod";
  */
 export const serverSchema = z.object({
     NEXTAUTH_SECRET: z.string(),
-    NEXTAUTH_URL: z.string().url(),
+    NEXTAUTH_URL: z.string().url().nullish(),
     //   DISCORD_CLIENT_ID: z.string(),
     //   DISCORD_CLIENT_SECRET: z.string(),
 });
