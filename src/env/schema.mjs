91a83291965992f22564577e3165c65bf5f92191
NikitoriusMinsk <45353693+NikitoriusMinsk@users.noreply.github.com>
2022-09-15 14:53:32 +0400

    register temp

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -20,6 +20,7 @@ export const serverSchema = z.object({
  */
 export const clientSchema = z.object({
     // NEXT_PUBLIC_BAR: z.string(),
+    NEXT_PUBLIC_API_URL: z.string(),
 });
 
 /**
@@ -30,4 +31,5 @@ export const clientSchema = z.object({
  */
 export const clientEnv = {
     // NEXT_PUBLIC_BAR: process.env.NEXT_PUBLIC_BAR,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
 };
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,29 +3,29 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { env } from "../../../env/server.mjs";
 
 export const authOptions: NextAuthOptions = {
-    pages: {
-        signIn: '/sign-in'
-    },
-    // Configure one or more authentication providers
-    providers: [
-        CredentialsProvider({
-            credentials: {
-                email: { label: "Email", type: "text", placeholder: "E-mail" },
-                password: { label: "Password", type: "password" }
-            },
-            async authorize(credentials, req) {
-                if ((credentials?.email === 'qwe@qwe.qwe') && (credentials.password === 'qwerty')) {
-                    // handle user auth here
-                    const user = { id: 1, name: "John Doe", email: credentials?.email }
-                    return user
-                } else {
-                    return null
-                }
-            },
-        }),
-    ],
-    debug: process.env.NODE_ENV === 'development',
-    secret: env.NEXTAUTH_SECRET
+	pages: {
+		signIn: "/sign-in",
+	},
+	// Configure one or more authentication providers
+	providers: [
+		CredentialsProvider({
+			credentials: {
+				email: { label: "Email", type: "text", placeholder: "E-mail" },
+				password: { label: "Password", type: "password" },
+			},
+			async authorize(credentials, req) {
+				if (credentials?.email === "qwe@qwe.qwe" && credentials.password === "qwerty") {
+					// handle user auth here
+					const user = { id: 1, name: "John Doe", email: credentials?.email };
+					return user;
+				} else {
+					return null;
+				}
+			},
+		}),
+	],
+	debug: process.env.NODE_ENV === "development",
+	secret: env.NEXTAUTH_SECRET,
 };
 
 export default NextAuth(authOptions);
